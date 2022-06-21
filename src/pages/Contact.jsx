956e22c58b03109e310f658e13cec7a8d5d3bb5b
Febriantara Muhammad <febriantaramuhamad@gmail.com>
2022-06-21 18:56:58 +0700

    komit 4

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, { useRef } from "react";
+import CommonSection from "../components/ui/Common-section/CommonSection";
+import { Container, Row, Col } from "reactstrap";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const nameRef = useRef("");
+  const emailRef = useRef("");
+  const subjectRef = useRef("");
+  const messageRef = useRef("");
 
-export default Contact
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <>
+      <CommonSection title="Contact" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6" className="m-auto text-center">
+              <h2>Drop a message</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+                tempora veniam, architecto dicta assumenda beatae quidem quos
+                earum numquam sequi!
+              </p>
+              <div className="contact mt-4">
+                <form onSubmit={handleSubmit}>
+                  <div className="form__input">
+                    <input
+                      type="text"
+                      name="name"
+                      placeholder="Enter your name"
+                      ref={nameRef}
+                    />
+                  </div>
+                  <div className="form__input">
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Enter your email"
+                      ref={emailRef}
+                    />
+                  </div>
+                  <div className="form__input">
+                    <input
+                      type="text"
+                      name="subject"
+                      placeholder="Enter your subject"
+                      ref={subjectRef}
+                    />
+                  </div>
+                  <div className="form__input">
+                    <textarea
+                      rows="7"
+                      placeholder="Write message"
+                      ref={messageRef}
+                    ></textarea>
+                  </div>
+                  <button
+                    className="send-btn"
+                    style={{
+                      border: "none",
+                      padding: "7px 25px",
+                      borderRadius: "5px",
+                      marginTop: "20px",
+                    }}
+                    type="submit"
+                  >
+                    Send message
+                  </button>
+                </form>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
+};
+
+export default Contact;
